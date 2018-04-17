25421692e7522bb37ac686b3a2b9261c5a517673
belst <git@bel.st>
2018-04-17 09:33:41 +0200

    Optimize

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,28 @@
 extern crate slug;
 
 use std::error::Error;
-use std::io::{self, Read, Write};
+use std::io::{self, BufRead, Write};
 
 fn normalize<S: AsRef<str>>(s: S) -> String {
     slug::slugify(s).replace('-', "")
 }
 
 fn main() {
-    let mut buffer = String::new();
-    io::stdin()
-        .read_to_string(&mut buffer)
-        .expect("Cannot read from stdin");
     let stdout = io::stdout();
     let mut handle = stdout.lock();
 
-    for s in buffer.lines() {
+    let stdin = io::stdin();
+
+    for s in stdin
+        .lock()
+        .lines()
+        .filter(Result::is_ok)
+        .map(Result::unwrap)
+    {
         let _ = handle
             .write(normalize(s).as_bytes())
             .and_then(|_| handle.write(b"\n"))
+            .and_then(|_| handle.flush()) // flush stdout
             .map_err(|e| eprintln!("{}", e.description()));
     }
 }
